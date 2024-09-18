d6d440361d97720aaa83c0d875b08fdb09b35e9f
Mateus Vieira <habdig7@gmail.com>
2024-09-18 19:02:22 -0300

    Boilerplate

---
+++ projeto.py
@@ -0,0 +1,7 @@
+print("#Filmes disponíveis: Filme 1, Filme 2 e Filme 3")
+
+t_filme=input("Qual filme deseja assistir?")
+
+print("#Os tipos de ingresso são: Inteira, Meia e VIP")
+
+t_ingresso= input("Qual ingresso você deseja?")
