d326412e985a2acb1407e3246cc9669cebe3d72b
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2024-09-18 19:18:32 -0300

    Add files via upload

--- projeto.py
+++ projeto.py
@@ -1,7 +1,10 @@
 print("#Filmes disponíveis: Filme 1, Filme 2 e Filme 3")
 
-t_filme=input("Qual filme deseja assistir?")
+t_filme=input("Qual filme deseja assistir? \n")
 
-print("#Os tipos de ingresso são: Inteira, Meia e VIP")
+print("#Os tipos de ingresso são: Inteira, Meia e VIP ")
 
-t_ingresso= input("Qual ingresso você deseja?")
+t_ingresso= input("Qual ingresso você deseja? \n")
+
+
+#Teste 123
