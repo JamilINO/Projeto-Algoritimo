5464096678247d175dc19540385970f00cc0e872
Joao Vitor <jvsouzarodello@gmailcom>
2024-09-18 21:33:36 -0300

    Aleluia

--- projeto.py
+++ projeto.py
@@ -1,10 +1,61 @@
-print("#Filmes disponíveis: Filme 1, Filme 2 e Filme 3")
+preco_base_1=20
+preco_base_2=15
+preco_base_3=10
 
-t_filme=input("Qual filme deseja assistir? \n")
+while (True):
 
-print("#Os tipos de ingresso são: Inteira, Meia e VIP ")
+    print("Filmes disponíveis: Filme 1, Filme 2 e Filme 3")
 
-t_ingresso= input("Qual ingresso você deseja? \n")
+    t_filme=input("Qual filme deseja assistir? \n")
+
+    print("Escolha entre sessão 1 e 2")
+
+    sessao=int(input("Qual a sessão:"))
+
+    print("Os tipos de ingresso são: Inteira, Meia e VIP ")
+
+    inteira=int(input("Quantas entradas inteiras:"))
+    meia=int(input("Quantas entradas meia:"))
+    vip=int(input("Quantas entradas vip:"))
+
+    endloop = input ("Deseja encerrar o atendimento:")
+    
+    if (t_filme.lower() == "filme 1" ):
+        inteira *= preco_base_1 
+        meia = (meia * preco_base_1) / 2 
+        vip = (vip * preco_base_1) * 1.5
+        print (f"R$ {inteira}")
+        print (f"R$ {meia}")
+        print (f"R$ {vip}")
+        if (sessao == 1):
+            sessao1_f1=True
+        elif (sessao == 2):
+            sessao2_f1=True 
+    elif (t_filme.lower() == "filme 2" ):
+        inteira *= preco_base_2 
+        meia = (meia * preco_base_2) / 2 
+        vip = (vip * preco_base_2) * 1.5
+        print (f"R$ {inteira}")
+        print (f"R$ {meia}")
+        print (f"R$ {vip}")
+        if (sessao == 1):
+            sessao1_f1=True
+        elif (sessao == 2):
+            sessao2_f1=True 
+    elif (t_filme.lower() == "filme 3" ):
+        inteira *= preco_base_3
+        meia = (meia * preco_base_3) / 2 
+        vip = (vip * preco_base_3) * 1.5
+        print (f"R$ {inteira}")
+        print (f"R$ {meia}")
+        print (f"R$ {vip}")
+        if (sessao == 1):
+            sessao1_f1=True
+        elif (sessao == 2):
+            sessao2_f1=True 
+    else:
+        print ("Error")
+    if (endloop.lower() == "sim"):
+        break 
 
 
-#Teste 123 oi mensagem do Ubuntu 
