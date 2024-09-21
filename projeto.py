e00c2166d3d237c90187bfba0fb9e158632eaf5a
Mateus Vieira <habdig7@gmail.com>
2024-09-21 20:39:11 -0300

    media avalicação adicionada

--- projeto.py
+++ projeto.py
@@ -2,6 +2,14 @@ preco_base_1=20
 preco_base_2=15
 preco_base_3=10
 
+
+media_f1 = 0
+it_f1 = 0
+media_f2 = 0
+it_f2 = 0
+media_f3 = 0
+it_f3 = 0
+
 while (True):
 
     print("Filmes disponíveis: Filme 1, Filme 2 e Filme 3")
@@ -38,18 +46,21 @@ while (True):
     
     avaliacao = int(input("Qual a avaliação desse filme:" ))
 
-    soma_ava = soma_ava + avaliacao
+    if t_filme.lower() == "filme 1":
+        media_f1 += avaliacao
+        it_f1 += 1
+    elif t_filme.lower() == "filme 2":
+        media_f2 += avaliacao
+        it_f2 += 1
+    elif t_filme.lower() == "filme 1":
+        media_f3 += avaliacao
+        it_f3 += 1
+
 
     if (t_filme.lower() == "filme 1" ):
-        
 
         inteira *= preco_base_1
-        
-
-
         meia = (meia * preco_base_1) / 2
-        
-
         vip = (vip * preco_base_1) * 1.5
         
             
@@ -68,25 +79,30 @@ while (True):
 
         
     endloop = input ("Deseja encerrar o atendimento:" )
+
+
+    print (t_filme," - Sessão", sessao, ":"  )
+    print ("Quantidade de ingressos vendidos")
+    print (f"-Inteira: {inteira_1}")
+    print (f"-Meia:{meia_1}")
+    print (f"-VIP:{vip_1}")
+    print ("Receita por tipo",sessao)
+    print (f"- Inteira: R$ {inteira:.2f}")
+    print (f"- Meia: R$ {meia:.2f}")
+    print (f"- VIP: R$ {vip:.2f}")
+    print ("Média de avaliações:" )
+    if it_f1 > 0:
+        print (f"Filme 1: {round(media_f1 / it_f1 )}" )
+    if it_f2 > 0:
+        print (f"Filme 2: {round(media_f2 / it_f2)}" )
+    if it_f3 > 0: 
+        print (f"Filme 3: {round(media_f3 / it_f3 )}" )
+
+    print (f"Total de ingresso vendidos: {inteira_1 + meia_1 + vip_1} " )
+    print ("Receita total do dia: R$", inteira+meia+vip )
+    
              
     if (endloop.lower() == "sim"):
-        
-            print (t_filme," - Sessão", sessao, ":"  )
-            print ("Quantidade de ingressos vendidos")
-            print (f"-Inteira: {inteira_1:.2f}")
-            print (f"-Meia:{meia_1:.2f}")
-            print (f"-VIP:{vip_1:.2f}")
-            print ("Receita por tipo",sessao)
-            print (f"- Inteira: R$ {inteira:.2f}")
-            print (f"- Meia: R$ {meia:.2f}")
-            print (f"- VIP: R$ {vip:.2f}")
-            print ("Média de avaliações:" )
-            print ("Filme 1:" )
-            print ("Filme 2:" )
-            print ("Filme 3:" )
-            print ("Total de ingresso vendidos:" )
-            print ("Receita total do dia: R$", inteira+meia+vip )
-    
             break
                 
    
